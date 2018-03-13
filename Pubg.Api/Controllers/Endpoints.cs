2fe613749c01798ef02ca5d43f13e2869caf0c0d
leewalkergm <leewalkergm@gmail.com>
2018-03-13 18:04:04 +0000

    Moved status out of regional endpoints

--- Pubg.Api/Controllers/Endpoints.cs
+++ Pubg.Api/Controllers/Endpoints.cs
@@ -1,55 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Pubg.Api.Models;
-using Pubg.Api.Generators;
 
 namespace Pubg.Api.Controllers
 {
-    [Produces("application/json")]
-    [Route("/shards/{shardId}")]
     public class Endpoints : Controller
     {
-        [HttpGet("matches")]
-        public IActionResult GetMatches()
-        {
-            var numMatches = 5;
-
-            var gen = new MatchGenerator();
-
-            var matches = new List<Match>(numMatches);
-
-            for (int i = 0; i < numMatches; i++)
-            {
-                matches.Add(gen.GenerateMatch());
-            }
-
-            return Ok(new Response
-            {
-                Data = matches
-            });
-        }
-
-        [HttpGet("matches/{matchId}")]
-        public IActionResult GetMatch(string matchId)
-        {
-            if (matchId == Guid.Empty.ToString())
-            {
-                return NotFound(new Response
-                {
-                    Data = new
-                    {
-                        Title = "Not Found"
-                    }
-                });
-            }
-
-            return Ok(new Response
-            {
-                Data = new MatchGenerator().GenerateMatch(matchId)
-            });
-        }
-
         [HttpGet("status")]
         public IActionResult GetStatus()
         {
---
+++ Pubg.Api/Controllers/RegionalEndpoints.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Pubg.Api.Models;
+using Pubg.Api.Generators;
+
+namespace Pubg.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("/shards/{shardId}")]
+    public class RegionalEndpoints : Controller
+    {
+        [HttpGet("matches")]
+        public IActionResult GetMatches()
+        {
+            var numMatches = 5;
+
+            var gen = new MatchGenerator();
+
+            var matches = new List<Match>(numMatches);
+
+            for (int i = 0; i < numMatches; i++)
+            {
+                matches.Add(gen.GenerateMatch());
+            }
+
+            return Ok(new Response
+            {
+                Data = matches
+            });
+        }
+
+        [HttpGet("matches/{matchId}")]
+        public IActionResult GetMatch(string matchId)
+        {
+            if (matchId == Guid.Empty.ToString())
+            {
+                return NotFound(new Response
+                {
+                    Data = new
+                    {
+                        Title = "Not Found"
+                    }
+                });
+            }
+
+            return Ok(new Response
+            {
+                Data = new MatchGenerator().GenerateMatch(matchId)
+            });
+        }
+    }
+}
